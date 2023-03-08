91859475ea8cf258f15e39b4037c9384ce4f86ff
Sam George <91432053+SamGeorge01@users.noreply.github.com>
2023-03-08 20:33:49 +0530

    Update 3

---
+++ RoomBookingSysytem1/Controllers/AdminController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RoomBookingSysytem1.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AddRoom()
+        {
+            return View();
+        }
+
+        public ActionResult ManageRoom()
+        {
+            return View();
+        }
+
+        public ActionResult BookingDetails()
+        {
+            return View();
+        }
+
+        public ActionResult NewAdmin()
+        {
+            return View();
+        }
+
+        public ActionResult ChangeAdminPass()
+        {
+            return View();
+        }
+    }
+}
---
+++ RoomBookingSysytem1/Models/AddRoomModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+
+namespace RoomBookingSysytem1.Models
+{
+    public class AddRoomModel
+    {
+        [Required(ErrorMessage = "Room ID is required.")]
+        [Display(Name = "Room ID")]
+        public int RoomID { get; set; }
+
+        [Required(ErrorMessage = "Room Name is required.")]
+        [Display(Name = "Room Name")]
+        public string RoomName { get; set; }
+
+        [Required(ErrorMessage = "Room Type is required.")]
+        [Display(Name = "Room Type")]
+        public string RoomType { get; set; }
+
+        [Required(ErrorMessage = "Number of seats is required.")]
+        [Display(Name = "Number Of Seats")]
+        public string NumberOfSeats { get; set; }
+
+        [Required(ErrorMessage = "Location is required.")]
+        [Display(Name = "Location")]
+        public string Location { get; set; }
+
+        [Required(ErrorMessage = "Price required.")]
+        [Display(Name = "Price")]
+        public string Price { get; set; }
+
+        [Required(ErrorMessage = "Image is required.")]
+        [Display(Name = "Image")]
+        public HttpPostedFileBase ImageFile { get; set; }
+    }
+}
