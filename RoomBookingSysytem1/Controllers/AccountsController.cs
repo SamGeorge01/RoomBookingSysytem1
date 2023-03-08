92a3f172c2638c0a499e7137ed4f322429d0ad67
Sam George <Sam George@513RR4-MSI-GF63>
2023-03-08 07:31:24 +0530

    Add project files.

---
+++ RoomBookingSysytem1/Controllers/AccountsController.cs
@@ -0,0 +1,91 @@
+﻿using RoomBookingSysytem1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RoomBookingSysytem1.Controllers
+{
+    public class AccountsController : Controller
+    {
+        // GET: Signup
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Signin()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Signup()
+        {
+            ViewBag.States = GetStates();
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Signup(SignupModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Save the user details to the database
+                // Redirect to the Signin page on successful signup
+                return RedirectToAction("Signin");
+            }
+
+            // If the model state is not valid, show the Signup view with error messages
+            ViewBag.States = GetStates();
+            return View(model);
+        }
+
+        private List<SelectListItem> GetStates()
+        {
+            // Return the list of states as select list items
+            return new List<SelectListItem>
+            {
+                new SelectListItem { Value = "KA", Text = "Karnataka" },
+                new SelectListItem { Value = "KL", Text = "Kerala" },
+                new SelectListItem { Value = "TN", Text = "Tamil Nadu" },
+            };
+        }
+
+
+
+        private Dictionary<string, List<string>> GetCitiesByState()
+        {
+            var citiesByState = new Dictionary<string, List<string>>();
+            citiesByState.Add("KA", new List<string> { "Bangalore", "Mangalore" });
+            citiesByState.Add("KL", new List<string> { "Kochi", "Thiruvananthapuram" });
+            citiesByState.Add("TN", new List<string> { "Chennai", "Tiruchirappalli" });
+            return citiesByState;
+        }
+
+        [HttpGet]
+        public JsonResult GetCities(string stateId)
+        {
+            var citiesByState = GetCitiesByState();
+            var cities = citiesByState.ContainsKey(stateId) ? citiesByState[stateId] : new List<string>();
+            var result = (from s in cities select new { id = s, text = s }).ToList();
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ RoomBookingSysytem1/Models/SignupModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RoomBookingSysytem1.Models
+{
+    public class SignupModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "First Name is required.")]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required.")]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Date of Birth is required.")]
+        [Display(Name = "Date of Birth")]
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required(ErrorMessage = "Gender is required.")]
+        public string Gender { get; set; }
+
+        [Required(ErrorMessage = "Phone Number is required.")]
+        [Display(Name = "Phone Number")]
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [Display(Name = "Email Address")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Address is required.")]
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "State is required.")]
+        public string State { get; set; }
+
+        [Required(ErrorMessage = "City is required.")]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "Username is required.")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Confirm Password is required.")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Password and Confirm Password must match.")]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
