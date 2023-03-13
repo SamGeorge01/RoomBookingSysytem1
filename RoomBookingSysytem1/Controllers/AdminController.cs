86e15b536f47a509b904d108fafa465b479155bb
Sam George <91432053+SamGeorge01@users.noreply.github.com>
2023-03-14 00:09:06 +0530

    Update

--- RoomBookingSysytem1/Controllers/AdminController.cs
+++ RoomBookingSysytem1/Controllers/AdminController.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using RoomBookingSysytem1.Models;
+using RoomBookingSysytem1.Service;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.ApplicationServices;
 using System.Web.Mvc;
 
 namespace RoomBookingSysytem1.Controllers
@@ -19,6 +22,29 @@ namespace RoomBookingSysytem1.Controllers
             return View();
         }
 
+        [HttpPost]
+        public ActionResult AddRoom(AddRoomModel addroomModel)
+        {
+            if (ModelState.IsValid)
+            {
+                // Save room details to the database
+                AddRoomService addroomService = new AddRoomService();
+                addroomService.AddRoom(addroomModel);
+
+                // Save room image to server
+                if (addroomModel.ImageFile != null && addroomModel.ImageFile.ContentLength > 0)
+                {
+                    string fileName = System.IO.Path.GetFileName(addroomModel.ImageFile.FileName);
+                    string path = System.IO.Path.Combine(Server.MapPath("~/Content/Images/Rooms/"), fileName);
+                    addroomModel.ImageFile.SaveAs(path);
+                }
+
+                return RedirectToAction("Index");
+            }
+
+            return View(addroomModel);
+        }
+  
         public ActionResult ManageRoom()
         {
             return View();
--- RoomBookingSysytem1/Service/AddRoomService.cs
+++ RoomBookingSysytem1/Service/AddRoomService.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using RoomBookingSysytem1.Models;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +11,46 @@ namespace RoomBookingSysytem1.Service
 {
     public class AddRoomService
     {
+        private readonly string connectionString = "Data Source= 513RR4-MSI-GF63; Database=RoomBooking; Integrated Security=True; Connect timeout=30; Encrypt=False;";
+        public bool AddRoom(AddRoomModel room)
+        {
+            bool result = false;
+            try
+            {
+                using (SqlConnection sqlCon = new SqlConnection(connectionString))
+                {
+                    sqlCon.Open();
+                    SqlCommand cmd = new SqlCommand("RoomDetails_CRUD", sqlCon);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@RoomName", room.RoomName);
+                    cmd.Parameters.AddWithValue("@RoomType", room.RoomType);
+                    cmd.Parameters.AddWithValue("@NumberOfSeats", room.NumberOfSeats);
+                    cmd.Parameters.AddWithValue("@Location", room.Location);
+                    cmd.Parameters.AddWithValue("@Price", room.Price);
+                    cmd.Parameters.AddWithValue("@Image", ConvertImageToBytes(room.ImageFile));
+                    cmd.Parameters.AddWithValue("@Action", "Insert");
+                    cmd.ExecuteNonQuery();
+                    result = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                // log or handle exception
+            }
+            return result;
+        }
+
+        private byte[] ConvertImageToBytes(HttpPostedFileBase image)
+        {
+            byte[] imageBytes = null;
+            if (image != null && image.ContentLength > 0)
+            {
+                using (var binaryReader = new BinaryReader(image.InputStream))
+                {
+                    imageBytes = binaryReader.ReadBytes(image.ContentLength);
+                }
+            }
+            return imageBytes;
+        }
     }
 }
